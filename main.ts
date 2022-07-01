eabf82e0810db7a0b4ef8446e20df2ee4554daf2
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-07-01 12:15:08 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
-basic.forever(function () {
-	
-})
+//% weight=100 color=#16A085 icon=T
+
+namespace temp {
+    /**
+     *  start the temp
+     */
+    //% block
+    //% group="temp"
+    export function startTemp(): void {
+
+        let temp = input.temperature()
+
+        basic.forever(function() {
+            
+            basic.showNumber(temp)
+        })
+    }
+    /**
+     *  value temp is useful
+     */
+    //% block
+    //% group="value"
+    export function tempValue(): number {
+        return input.temperature()
+    }
+}
